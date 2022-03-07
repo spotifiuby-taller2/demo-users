00f1397bd7a0c04de815cfd8276e3c826b0a107c
Esteban <edjeordjian@fi.uba.ar>
2022-03-07 18:16:20 -0300

    Trying to fix database error

--- src/data/database.js
+++ src/data/database.js
@@ -3,19 +3,27 @@ const constants = require("../others/constants");
 
 const database = new Sequelize(constants.databaseUrl, {
   dialect: 'postgres',
+
   logging: false,
+
   operatorsAliases: Sequelize.Op,
+
   define: {
     timestamp: false
   },
+
   ssl: true,
+
   pool: {
     max: 100,
     min: 0,
     idle: 200000,
     acquire: 1000000
   },
-  dialectOptions: {},
+
+  dialectOptions: {
+    ssl: true
+  },
 } );
 
 module.exports = database;
